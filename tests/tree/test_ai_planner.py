09a94dae21537bc725bec38a27098cc1a70eff8e
Nicholas Goodman <ngoodman@bayontechnologies.com>
2026-02-20 11:24:34 -0700

    Add Apache 2.0 license headers to all Python files and LICENSE file

--- tests/tree/test_ai_planner.py
+++ tests/tree/test_ai_planner.py
@@ -1,3 +1,17 @@
+# Copyright 2026 Pennyworth Technologies, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Tests for tree/ai_planner.py - _build_tree_from_json and prompt generation."""
 
 from agent_arborist.tree.ai_planner import _build_tree_from_json
